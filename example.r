efabea7560e16ac1318c6bc515dd016d1defb09b
cigrainger <cigrainger@gmail.com>
2014-02-12 12:57:51 +0000

    Create homework

---
+++ example.r
@@ -0,0 +1,15 @@
+# Load the files
+
+# Get the data into long form
+
+# Merge the data
+
+# Generate summary statistics of the data
+
+# By country
+
+# By indicator
+
+# Save the data
+
+# Extra credit: create a plot or two
